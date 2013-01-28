fbd5a9ea867a48dcfb62c24b362581492a99ceca
Vitor Freitas <vitorfs@gmail.com>
2013-01-27 22:18:25 -0200

    added verbose name to models

--- exam_assign/models.py
+++ exam_assign/models.py
@@ -1,4 +1,4 @@
-from django.db import models
+﻿from django.db import models
 from django.contrib.auth.models import User
 from exams.models import Exam, Question, Answer
 
@@ -6,8 +6,15 @@ class UserExam(models.Model):
     user = models.ForeignKey(User)
     exam = models.ForeignKey(Exam)
     expire_date = models.DateTimeField()
-    start_time = models.DateTimeField()
-    end_time = models.DateTimeField()
+    start_time = models.DateTimeField(null=True, blank=True)
+    end_time = models.DateTimeField(null=True, blank=True)
+
+    def __unicode__(self):
+         return u"%s %s" % (self.user, self.exam)
+
+    class Meta:
+        verbose_name = "Prova de Usuário"
+        verbose_name_plural = "Provas de Usuários"
 
 class UserExamAnswers(models.Model):
     user_exam = models.ForeignKey(UserExam)
--- exams/models.py
+++ exams/models.py
@@ -4,7 +4,11 @@ class QuestionSubject(models.Model):
     subject = models.CharField(max_length=50)
 
     def __unicode__(self):
-        return self.subject    
+        return self.subject
+
+    class Meta:
+        verbose_name = "Assunto de Questão"
+        verbose_name_plural = "Assuntos de Questões"
 
 class QuestionDificulty(models.Model):
     dificulty = models.CharField(max_length=50)
@@ -13,11 +17,19 @@ class QuestionDificulty(models.Model):
     def __unicode__(self):
         return self.dificulty
 
+    class Meta:
+        verbose_name = "Dificuldade de Questão"
+        verbose_name_plural = "Dificuldades de Questões"
+
 class QuestionType(models.Model):
     type = models.CharField(max_length=30)
 
     def __unicode__(self):
         return self.type
+
+    class Meta:
+        verbose_name = "Tipo de Questão"
+        verbose_name_plural = "Tipos de Questões"
 
 class Question(models.Model):
     question = models.CharField(max_length=200)
@@ -44,11 +56,19 @@ class Answer(models.Model):
     def __unicode__(self):
         return self.answer
 
+    class Meta:
+        verbose_name = "Resposta"
+        verbose_name_plural = "Respostas"
+
 class ExamSubject(models.Model):
     subject = models.CharField(max_length=50)
 
     def __unicode__(self):
-        return self.subject    
+        return self.subject
+
+    class Meta:
+        verbose_name = "Assunto de Prova"
+        verbose_name_plural = "Assuntos de Provas"
 
 class Exam(models.Model):
     name = models.CharField(max_length=50)
@@ -58,4 +78,8 @@ class Exam(models.Model):
     duration = models.IntegerField()
 
     def __unicode__(self):
-        return self.name        
+        return self.name
+
+    class Meta:
+        verbose_name = "Prova"
+        verbose_name_plural = "Provas"
