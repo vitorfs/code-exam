8d6fb7241b3693835f41fb415f5f55a483849fa4
Vitor Freitas <vitorfs@gmail.com>
2013-01-29 23:58:29 -0200

    updated gitignore, deleted old folders

--- core/urls.py
+++
@@ -1,8 +0,0 @@
-# coding: utf-8
-from django.conf.urls import patterns, include, url
-from core.views import Homepage
-
-
-urlpatterns = patterns('eventex.core.views',
-    url(r'^$', Homepage.as_view(), name='homepage'),
-)
--- core/views.py
+++
@@ -1,8 +0,0 @@
-# coding: utf-8
-from datetime import time
-from django.views.generic.simple import direct_to_template
-from django.shortcuts import get_object_or_404
-from django.views.generic import TemplateView, DetailView
-
-class Homepage(TemplateView):
-    template_name = 'index.html'
