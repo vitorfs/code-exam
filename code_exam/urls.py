b6f6874e028851ab58cec8c0ef6967d4758290f0
Vitor Freitas <vitorfs@gmail.com>
2013-02-04 01:05:11 -0200

    user answer now persisting

--- code_exam/urls.py
+++ code_exam/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from home.views import homepage, login, logout, exam, question
+from home.views import homepage, login, logout, exam, question, send_answer
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 admin.autodiscover()
@@ -11,6 +11,7 @@ urlpatterns = patterns('',
     (r'^logout/', logout),
     url(r'^prova/(?P<exam_id>\d+)/$', exam),
     url(r'^prova/(?P<exam_id>\d+)/questao/(?P<question_id>\d+)/$', question),
+    (r'^send-answer/', send_answer),
     url(r'^admin/', include(admin.site.urls)),
 )
 
--- home/views.py
+++ home/views.py
@@ -1,7 +1,8 @@
 # coding: utf-8
+from django.http import HttpResponse
 from django.shortcuts import render_to_response, get_object_or_404, redirect
 from django.template import RequestContext
-from exams.models import UserExam, Question
+from exams.models import UserExam, Question, Answer, UserExamAnswers
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login as django_login, logout as django_logout
 
@@ -14,15 +15,46 @@ def homepage(request):
         return redirect('/login/')
 
 def exam(request, exam_id):
-    user_exam = UserExam.objects.get(pk=int(exam_id))
-    context = RequestContext(request, { 'user_exam': user_exam })
-    return render_to_response('home/exam.html', context)
+    user_exam = UserExam.objects.get(pk=exam_id)
+    if (request.user.id == user_exam.user.id):
+        context = RequestContext(request, { 'user_exam': user_exam })
+        return render_to_response('home/exam.html', context)
+    else:
+        context = RequestContext(request, {'message_severity': 'error', 'message': 'Usuário sem permissão para acessar prova.'})
+        return render_to_response('home/exam.html', context)
 
 def question(request, exam_id, question_id):
-    user_exam = UserExam.objects.get(pk=int(exam_id))
-    question = Question.objects.get(pk=int(question_id))
-    context = RequestContext(request, { 'user_exam': user_exam, 'question': question })
-    return render_to_response('home/question.html', context)
+    user_exam = UserExam.objects.get(pk=exam_id)
+    if (request.user.id == user_exam.user.id):
+        question = Question.objects.get(pk=question_id)
+        answers = Answer.objects.filter(question__id=question_id)
+        try:
+            user_answer = UserExamAnswers.objects.get(user_exam__id=exam_id, question__id=question_id)
+        except:
+            user_answer = None
+        context = RequestContext(request, { 'user_exam': user_exam, 
+            'question': question, 
+            'answers': answers, 
+            'user_answer': user_answer, 
+            })
+        return render_to_response('home/question.html', context)
+    else:
+        context = RequestContext(request, {'message_severity': 'error', 'message': 'Usuário sem permissão para acessar prova.'})
+        return render_to_response('home/exam.html', context)
+
+def send_answer(request):
+    if request.method == 'POST':
+        user_exam_id = request.POST['user-exam-id']
+        question_id = request.POST['question-id']
+        answer_id = request.POST['rb-answer']
+
+        user_exam_answer = UserExamAnswers.objects.filter(user_exam__id=user_exam_id, question__id=question_id)
+        user_exam_answer.delete()
+
+        user_exam_answer = UserExamAnswers(user_exam=UserExam(id=user_exam_id), question=Question(id=question_id), closed_answer=Answer(id=answer_id))
+        user_exam_answer.save()
+
+        return HttpResponse('sucesso')
 
 def login(request):
     if request.method == 'POST':
